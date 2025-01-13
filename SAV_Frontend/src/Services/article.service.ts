d9b8d9547e5ec0ec3944255a500facb23c118442
yasmine ghorbel <yasmineghorbel29@gmail.com>
2025-01-13 19:02:44 +0100

    update

--- SAV_Frontend/src/Services/article.service.ts
+++ SAV_Frontend/src/Services/article.service.ts
@@ -1,28 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Evt } from 'src/Models/Event';
+import { Article } from '../Models/Article';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventService {
+export class ArticleService {
   constructor(private http:HttpClient) { }
-  getAllEvents():Observable<Evt[]>{
+  getAllEvents():Observable<Article[]>{
     //type de retourne est une observable qui contient la liste des members
-    return this.http.get<Evt[]>('http://localhost:3000/event')
+    return this.http.get<Article[]>('http://localhost:3000/event')
   }
-  getEvent(id:String):Observable<Evt>{
+  getEvent(id:String):Observable<Article>{
     //type de retourne est une observable qui contient la liste des members
-    return this.http.get<Evt>(`http://localhost:3000/event/${id}`)
+    return this.http.get<Article>(`http://localhost:3000/event/${id}`)
   }
   delete(eventId:string):Observable<void>{
     return this.http.delete<void>(`http://localhost:3000/event/${eventId}`)
   }
-  add(event:Evt):Observable<void>{
+  add(event:Article):Observable<void>{
     return this.http.post<void>('http://localhost:3000/event',event)
   }
-  edit(id:string,event:Evt):Observable<void>{
+  edit(id:string,event:Article):Observable<void>{
     return this.http.put<void>(`http://localhost:3000/event/${id}`,event)
 
   }
